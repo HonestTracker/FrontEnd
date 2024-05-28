834458a3312202d02161085a4d9657eb981d60a8
cerchio22 <cerchiogerrits10@gmail.com>
2024-05-28 09:58:42 +0200

    Router
    map voor router aangemaakt met functioneel werkende paginas

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import AppRouter from "./Router";
 
 function App() {
   return (
     <div className="container">
       <h1 className="text-4xl font-bold">Welkom bij mijn React-project</h1>
+
+      <AppRouter />
     </div>
   );
 }

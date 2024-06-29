211c30cd8c6a2c4c697952d4588adaee4707d71d
Nirahz <90685684+TGcoolman@users.noreply.github.com>
2024-06-29 16:31:27 +0200

    JK found the issue
    :D

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import AppRouter from "./utils/navigation/Router";
 import "./utils/constants/styles/globals.css";
+import "@fontsource/poppins";
 
 function App() {
   return (

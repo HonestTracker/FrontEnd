cc6e5261e06d812e2ff35b9bbf9f273205489817
Nirahz <90685684+TGcoolman@users.noreply.github.com>
2024-06-29 11:05:27 +0200

    byebyeb

--- app/src/Router.js
+++ app/src/Router.js
@@ -6,9 +6,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFoundPage from "./pages/404Page";
 import About from "./pages/About";
-
 import Contact from "./pages/Contact";
-import ProductTracking from "./pages/ProductTracking";
 import ProductDetails from "./pages/ProductDetails";
 import CommentsPage from "./pages/CommentsPage";
 import Settings from "./pages/Settings";
@@ -17,6 +15,7 @@ import Navbar from "./utils/components/navigation/Navbar";
 import Footer from "./utils/components/navigation/Footer";
 import SettingsAuthenticated from "./pages/SettingsAuthenticated";
 import ProfilePage from "./pages/ProfilePage";
+
 function AppRouter() {
   return (
     <Router>
@@ -28,7 +27,6 @@ function AppRouter() {
         <Route path="/about" element={<About />} />
         <Route path="/comments" element={<CommentsPage />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/productTracking" element={<ProductTracking />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/settingsauth" element={<SettingsAuthenticated />} />
         <Route path="/products" element={<ProductOverview />} />
